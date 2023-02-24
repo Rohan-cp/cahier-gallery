bb0d2ffd30cfce1d7d96a2f5c782e6a7a247f630
Rohan-cp <rohancp9@icloud.com>
2023-02-24 05:19:09 -0500

    base and script running

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import image1 from "./assets/1.jpeg";
+import image2 from "./assets/2.jpeg";
+import image3 from "./assets/3.jpeg";
+import image4 from "./assets/4.jpeg";
+import image5 from "./assets/5.jpeg";
+import image6 from "./assets/6.jpeg";
+import image7 from "./assets/7.jpeg";
+import image8 from "./assets/8.jpeg";
+import useScript from "./use-script";
+import "./App.css";
 
 function App() {
+  console.log("hereeeeee");
+  // const status = useScript(require('./script.js'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        <div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+          <img alt="" className="image" src={image1} draggable="false" />
+          <img alt="" className="image" src={image2} draggable="false" />
+          <img alt="" className="image" src={image3} draggable="false" />
+          <img alt="" className="image" src={image4} draggable="false" />
+          <img alt="" className="image" src={image5} draggable="false" />
+          <img alt="" className="image" src={image6} draggable="false" />
+          <img alt="" className="image" src={image7} draggable="false" />
+          <img alt="" className="image" src={image8} draggable="false" />
+        </div>
+      }
     </div>
   );
 }
